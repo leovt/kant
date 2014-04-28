ff1d8e37be0951b93e13d6578b74bab2588ddd81
leovt <leonhard.vogt@gmx.ch>
2014-04-28 11:23:36 +0200

    added LET statement

--- basic/basic.py
+++ basic/basic.py
@@ -6,14 +6,14 @@ src = '''
 20 DIM NAME AS STRING
 30 INPUT "WHATS YOUR NAME? " ; NAME
 40 INPUT "WHATS YOUR AGE? " ; I
-50 I = I - 1
+50 LET I = I - 1
 60 IF I = 0 THEN GOTO 90
 70 PRINT "HELLO," ; NAME ; I
 80 GOTO 50
-90 NAME = NAME + "E"
-95 I = I = 1
+90 LET NAME = NAME + "E"
+95 LET I = I = 1
 96 PRINT "SHOULD BE ZERO"; I
-97 I = NAME = "NAME"
+97 LET I = NAME = "NAME"
 98 PRINT I; NAME
 99 PRINT "" = ""
 100 END
@@ -129,9 +129,11 @@ def stmt(tokens, lineno, context):
     elif tokens[0] == 'END':
         return ('end',)
 
-    elif tokens[1] == '=':
-        v_type, v_id = context.symbols[variable(tokens[0], lineno)]
-        return ('assign', v_type, v_id, expr(tokens[2:], lineno, context))
+    elif tokens[0] == 'LET':
+        v_type, v_id = context.symbols[variable(tokens[1], lineno)]
+        if tokens[2] != '=':
+            error('LET expected = sign for assignment')
+        return ('assign', v_type, v_id, expr(tokens[3:], lineno, context))
 
     else:
         error('unrecognized statement %s on line %d' % (' '.join(tokens), lineno))
